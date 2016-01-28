05ef0d0f19f06786938fe945592d010c366bdba1
rogi <rogi@Zipcoder26s-MacBook-Pro.local>
2016-01-28 13:27:09 -0500

    created 2 classes colorful thing and Rainbowcolor_enum

---
+++ ColorfulThing.java
@@ -0,0 +1,26 @@
+package nangelimalil.rogi.initLab02;
+import java.util.*;
+/**
+ * Created by rogi on 1/28/16.
+ */
+public class ColorfulThing {
+        RainBowColor rnbclr;
+
+        ColorfulThing(RainBowColor rnbclr) {
+            this.rnbclr = rnbclr;
+        }
+
+        public RainBowColor getColor() {
+            return this.rnbclr;
+        }
+
+/*    public boolean equals(ColorfulThing c) {
+    return rnbclr.equals(c.getColor());
+    }
+
+    public boolean equals(RainBowColor r) {
+    return rnbclr.equals(r);
+    }
+*/
+    }
+
--- InitializationLab02.java
+++ InitializationLab02.java
@@ -1,11 +1,18 @@
 package nangelimalil.rogi.initLab02;
-
+import java.util.Objects;
 /**
  * Created by rogi on 1/28/16.
  */
+
+
+enum RainBowColor {
+    Violete, Indigo, Blue, Green, Yellow, Orange, Red
+}
+
+
+
+
 public class InitializationLab02 {
 
-
-
-
 }
+
